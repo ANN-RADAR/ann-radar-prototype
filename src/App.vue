07e47fb9644b3baa3d022e838ca2930b6683cc6e
Jan Behrens <36447927+janbehrens@users.noreply.github.com>
2021-11-12 15:12:11 +0100

    linter config

--- src/App.vue
+++ src/App.vue
@@ -171,30 +171,37 @@
                           :fixed-header="true"
                           hide-default-footer
                         >
+                          <!-- eslint-disable-next-line vue/valid-v-slot -->
                           <template v-slot:item.AnzFl="{ item }">
                             <span v-if="item.AnzFl !== undefined">{{ formatNumber(Math.round(item.AnzFl)) }}</span>
                             <span v-else>k. A.</span>
                           </template>
+                          <!-- eslint-disable-next-line vue/valid-v-slot -->
                           <template v-slot:item.mittlFl="{ item }">
                             <span v-if="item.mittlFl !== undefined">{{ formatNumber(Math.round(item.mittlFl)) }}&nbsp;m²</span>
                             <span v-else>k. A.</span>
                           </template>
+                          <!-- eslint-disable-next-line vue/valid-v-slot -->
                           <template v-slot:item.BGF="{ item }">
                             <span v-if="item.BGF !== undefined">{{ formatNumber(Math.round(item.BGF)) }}&nbsp;m²</span>
                             <span v-else>k. A.</span>
                           </template>
+                          <!-- eslint-disable-next-line vue/valid-v-slot -->
                           <template v-slot:item.tatNu_WB_P="{ item }">
                             <span v-if="item.tatNu_WB_P !== undefined">{{ formatNumber(item.tatNu_WB_P) }}&nbsp;%</span>
                             <span v-else>k. A.</span>
                           </template>
+                          <!-- eslint-disable-next-line vue/valid-v-slot -->
                           <template v-slot:item.Bev_311219="{ item }">
                             <span v-if="item.Bev_311219 !== undefined">{{ formatNumber(Math.round(item.Bev_311219)) }}</span>
                             <span v-else>k. A.</span>
                           </template>
+                          <!-- eslint-disable-next-line vue/valid-v-slot -->
                           <template v-slot:item.p_st_mwh_a="{ item }">
                             <span v-if="item.p_st_mwh_a !== undefined">{{ formatNumber(item.p_st_mwh_a) }}&nbsp;MWh/a</span>
                             <span v-else>k. A.</span>
                           </template>
+                          <!-- eslint-disable-next-line vue/valid-v-slot -->
                           <template v-slot:item.Soz_Status="{ item }">
                             <span v-if="item.Soz_Status !== undefined">{{ item.Soz_Status }}</span>
                             <span v-else>k. A.</span>
