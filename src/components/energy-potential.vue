dec5d86a40d92c111fdd835c63b5922d212a96f7
Frank Mecklenburg <frank@yourfriendlyrobot.net>
2022-02-09 12:48:58 +0100

    feat(energy): map size 50%

--- src/components/energy-potential.vue
+++ src/components/energy-potential.vue
@@ -85,7 +85,7 @@ export default Vue.extend({
 <style scoped>
 .wrapper {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 50% 1fr 1fr;
   grid-template-rows: 1fr 1fr 1fr;
   gap: 0.5rem;
   padding: 0.5rem;
