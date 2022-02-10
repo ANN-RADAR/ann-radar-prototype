45af37c637257ccfae1ec9024e08065751463600
Frank Mecklenburg <frank@yourfriendlyrobot.net>
2022-02-10 16:40:18 +0100

    refactor(admin areas): move style function to lib

--- src/constants/layers.ts
+++ src/constants/layers.ts
@@ -1,3 +1,4 @@
+import {getAdminAreaStyle} from '@/libs/admin-area';
 import {Baublock} from '@/models/Baublock';
 import {Bezirk} from '@/models/Bezirk';
 import {Stadt} from '@/models/Stadt';
@@ -6,10 +7,6 @@ import {StatGebiet} from '@/models/StatGebiet';
 import LayerGroup from 'ol/layer/Group';
 import TileLayer from 'ol/layer/Tile';
 import VectorLayer from 'ol/layer/Vector';
-import Fill from 'ol/style/Fill';
-import Stroke from 'ol/style/Stroke';
-import Style, {StyleFunction} from 'ol/style/Style';
-import Text from 'ol/style/Text';
 import {tileSources, vectorSources} from './sources';
 
 export const thematicLayers = [
@@ -66,39 +63,6 @@ export const mapStyleLayers = [
     source: tileSources.HH_WMS_Geobasiskarten_SG
   }
 ];
-
-const getAdminAreaStyle =
-  (layerName: string, textAttr: string): StyleFunction =>
-  feature =>
-    feature.get('selected')
-      ? new Style({
-          stroke: new Stroke({
-            color: '#f00',
-            width: 2
-          }),
-          fill: new Fill({
-            color: 'rgba(255, 0, 0, 0.1)'
-          }),
-          text: new Text({
-            font: '16px Arial',
-            text: feature.get(textAttr),
-            fill: new Fill({color: 'black'}),
-            stroke: new Stroke({color: '#fee', width: 4})
-          }),
-          zIndex: 1
-        })
-      : new Style({
-          stroke: new Stroke({
-            color: 'rgba(0, 120, 255, 1.0)',
-            width: 2
-          }),
-          text: new Text({
-            font: '16px Arial',
-            text: feature.get(textAttr),
-            fill: new Fill({color: 'black'}),
-            stroke: new Stroke({color: '#eee', width: 4})
-          })
-        });
 
 const adminAreaLayers = [
   {
