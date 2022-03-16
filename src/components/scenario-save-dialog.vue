802b902e530806ff08c264410f8ca87462ade340
Leslie Zimmermann <lesliez@gmx.de>
2022-03-16 13:56:02 +0100

    feat(scenarios): add save scenario dialog

---
+++ src/components/scenario-save-dialog.vue
@@ -0,0 +1,162 @@
+<template>
+  <v-dialog v-model="open" max-width="600px">
+    <template v-slot:activator="{on, attrs}">
+      <v-btn
+        v-bind="attrs"
+        v-on="on"
+        :disabled="
+          !Object.values(adminLayerData).some(
+            ({selectedFeatureDataKeys, note}) =>
+              selectedFeatureDataKeys.length || note
+          ) && !baseLayerTypes.length
+        "
+      >
+        {{ $t('scenarios.saveScenario') }}
+      </v-btn>
+    </template>
+
+    <v-card>
+      <v-form v-model="isFormValid">
+        <v-card-title>{{ $t('scenarios.saveScenario') }}</v-card-title>
+        <v-card-text>
+          <v-text-field
+            :label="$t('scenarios.scenarioTitle')"
+            v-model="scenarioTitle"
+            :rules="scenarioTitleRules"
+            filled
+          ></v-text-field>
+
+          <p>
+            {{
+              $tc(
+                'scenarios.adminAreasConfigured',
+                Object.values(adminLayerData).filter(
+                  ({selectedFeatureDataKeys, note}) =>
+                    selectedFeatureDataKeys.length || note
+                ).length
+              )
+            }}
+            <br />
+            {{ $tc('scenarios.scenarioLayers', baseLayerTypes.length) }}
+          </p>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="open = false">
+            {{ $t('cancel') }}
+          </v-btn>
+
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="saveScenario()"
+            :loading="isSaving"
+            :disabled="!isFormValid || isSaving"
+          >
+            {{ $t('scenarios.saveScenario') }}
+            <template v-slot:loader>
+              <span>{{ $t('saving') }}</span>
+            </template>
+          </v-btn>
+        </v-card-actions>
+      </v-form>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {mapState} from 'vuex';
+import {MapStateToComputed} from '@/types/store';
+import {Scenario} from '@/types/scenario';
+
+interface Data {
+  open: boolean;
+  isFormValid: boolean;
+  isSaving: boolean;
+  scenarioTitle: string;
+}
+
+export default Vue.extend({
+  props: {
+    scope: {
+      type: String,
+      required: true
+    }
+  },
+  data(): Data {
+    return {
+      open: false,
+      isFormValid: false,
+      isSaving: false,
+      scenarioTitle: ''
+    };
+  },
+  computed: {
+    ...(mapState as MapStateToComputed)('root', [
+      'adminLayerData',
+      'baseLayerTypes'
+    ]),
+    scenarioTitleRules(): Array<(() => boolean | string) | boolean | string> {
+      // Get saved scenarios
+      const storageString = localStorage.getItem('scenarios');
+      const allSavedScenarios: Record<
+        string,
+        Array<Scenario>
+      > = (storageString && JSON.parse(storageString)) || {};
+      const savedScenarios = allSavedScenarios[this.scope] || [];
+
+      // Check if scenario title already exists
+      const scenarioTitleAlreadyExists = savedScenarios.some(
+        scenario => scenario.scenarioTitle === this.scenarioTitle
+      );
+      const scenarioTitleValidation = scenarioTitleAlreadyExists
+        ? this.$t('scenarios.error.scenarioTitleAlreadyExists')
+        : true;
+
+      return [scenarioTitleValidation];
+    }
+  },
+  watch: {
+    open(newOpenState: boolean) {
+      // Reset the dialog when it was closed
+      if (!newOpenState) {
+        this.resetDialog();
+      }
+    }
+  },
+  methods: {
+    resetDialog() {
+      this.scenarioTitle = '';
+      this.isSaving = false;
+    },
+    saveScenario() {
+      this.isSaving = true;
+
+      // Get saved scenarios
+      const storageString = localStorage.getItem('scenarios');
+      const allSavedScenarios: Record<
+        string,
+        Array<Scenario>
+      > = (storageString && JSON.parse(storageString)) || {};
+
+      // Add current scenario
+      const scenario: Scenario = {
+        scenarioTitle: this.scenarioTitle,
+        adminLayerData: this.adminLayerData,
+        baseLayerTypes: this.baseLayerTypes
+      };
+      allSavedScenarios[this.scope] = [
+        ...allSavedScenarios[this.scope],
+        scenario
+      ];
+
+      // Save scenarios in local storage
+      localStorage.setItem('scenarios', JSON.stringify(allSavedScenarios));
+
+      // Close the dialog
+      this.open = false;
+    }
+  }
+});
+</script>
--- src/views/category-solar.vue
+++ src/views/category-solar.vue
@@ -5,13 +5,20 @@
       fluid
       style="display: flex; flex-direction: column; height: 100%; padding: 0"
     >
-      <v-tabs v-model="tab">
-        <v-tab>{{ $t('navigation.potential') }}</v-tab>
-        <v-tab>{{ $t('navigation.plans') }}</v-tab>
-        <v-tab>{{ $t('navigation.stakeholders') }}</v-tab>
-        <v-tab>{{ $t('navigation.urbanData') }}</v-tab>
-        <v-tab>{{ $t('navigation.governance') }}</v-tab>
-      </v-tabs>
+      <div class="solar-header">
+        <v-tabs v-model="tab">
+          <v-tab>{{ $t('navigation.potential') }}</v-tab>
+          <v-tab>{{ $t('navigation.plans') }}</v-tab>
+          <v-tab>{{ $t('navigation.stakeholders') }}</v-tab>
+          <v-tab>{{ $t('navigation.urbanData') }}</v-tab>
+          <v-tab>{{ $t('navigation.governance') }}</v-tab>
+        </v-tabs>
+
+        <div class="solar-header-scenario-actions">
+          <ScenarioSaveDialog scope="solar-potential" />
+        </div>
+      </div>
+
       <v-tabs-items v-model="tab" style="height: 100%">
         <v-tab-item style="flex-grow: 1">
           <Potential />
@@ -26,13 +33,14 @@ import Vue from 'vue';
 
 import Potential from '../components/solar-potential.vue';
 import Header from '../components/app-header.vue';
+import ScenarioSaveDialog from '../components/scenario-save-dialog.vue';
 
 export default Vue.extend({
   components: {
     Potential,
-    Header
+    Header,
+    ScenarioSaveDialog
   },
-
   data() {
     return {
       tab: 0
@@ -40,3 +48,17 @@ export default Vue.extend({
   }
 });
 </script>
+
+<style scoped>
+.solar-header {
+  display: grid;
+  grid-template-columns: 1fr auto;
+}
+
+.solar-header-scenario-actions {
+  display: flex;
+  align-items: center;
+  grid-gap: 8px;
+  padding-right: 16px;
+}
+</style>
