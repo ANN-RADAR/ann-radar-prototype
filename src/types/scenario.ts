f5e34af98c09a63e35fb1a470138e54fec939f5c
Leslie Zimmermann <lesliez@gmx.de>
2022-04-29 14:03:29 +0200

    refactor(scenarios): remove deprecated scenario actions from potential views

--- src/types/scenario.ts
+++
@@ -1,7 +0,0 @@
-import {AdminLayerData, AdminLayerType} from './admin-layers';
-
-export interface Scenario {
-  scenarioTitle: string;
-  adminLayerData: Record<AdminLayerType, AdminLayerData>;
-  baseLayerTypes: Array<string>;
-}
