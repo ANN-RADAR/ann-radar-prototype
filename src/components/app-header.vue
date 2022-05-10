ed9d4e67d024121094227d811484dfef8422d6da
Leslie Zimmermann <lesliez@gmx.de>
2022-05-10 08:26:16 +0200

    feat(notes): add notes drawer

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,16 +1,33 @@
 <template>
   <v-app-bar app elevation="2" dense class="header">
-    <v-tabs v-model="tab">
-      <v-tab to="/potential">{{ $t('navigation.potential') }}</v-tab>
-      <v-tab to="/plans">{{ $t('navigation.plans') }}</v-tab>
-      <v-tab to="/stakeholders">{{ $t('navigation.stakeholders') }}</v-tab>
-      <v-tab to="/urban-data">{{ $t('navigation.urbanData') }}</v-tab>
-      <v-tab to="/governance">{{ $t('navigation.governance') }}</v-tab>
-    </v-tabs>
+    <v-app-bar-title>{{ $t('annRadar') }}</v-app-bar-title>
 
-    <h1 class="headline">{{ $t('annRadar') }}</h1>
+    <div class="header-actions">
+      <v-btn
+        text
+        active-class="primary--text"
+        :input-value="showNotes"
+        @click="$emit('toggleNotes')"
+      >
+        <span>{{ $t('notes') }}</span>
+        <v-icon right>mdi-notebook-edit-outline</v-icon>
+      </v-btn>
 
-    <v-btn @click="logOut">{{ $t('auth.logout') }}</v-btn>
+      <v-btn text @click="logOut">
+        <span>{{ $t('auth.logout') }}</span>
+        <v-icon right>mdi-logout</v-icon>
+      </v-btn>
+    </div>
+
+    <template v-slot:extension>
+      <v-tabs v-model="tab">
+        <v-tab to="/potential">{{ $t('navigation.potential') }}</v-tab>
+        <v-tab to="/plans">{{ $t('navigation.plans') }}</v-tab>
+        <v-tab to="/stakeholders">{{ $t('navigation.stakeholders') }}</v-tab>
+        <v-tab to="/urban-data">{{ $t('navigation.urbanData') }}</v-tab>
+        <v-tab to="/governance">{{ $t('navigation.governance') }}</v-tab>
+      </v-tabs>
+    </template>
   </v-app-bar>
 </template>
 
@@ -18,8 +35,18 @@
 import Vue from 'vue';
 import {logOut} from '@/libs/firebase';
 
+interface Data {
+  tab: number;
+}
+
 export default Vue.extend({
-  data() {
+  props: {
+    showNotes: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data(): Data {
     return {
       tab: 0
     };
@@ -35,9 +62,18 @@ export default Vue.extend({
 </script>
 
 <style scoped>
+header.header {
+  z-index: 8;
+}
+
 .header::v-deep > div {
   display: grid;
   grid-template-columns: 1fr auto auto;
-  grid-gap: 16px;
+}
+
+.header-actions {
+  display: grid;
+  grid-auto-flow: column;
+  grid-gap: 8px;
 }
 </style>
