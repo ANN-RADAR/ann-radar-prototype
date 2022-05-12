7cce72dfb9fd2c62e0979d2ddacb27ccc4642605
Frank Mecklenburg <frank@yourfriendlyrobot.net>
2022-05-12 16:21:08 +0200

    feat(laboratories): add wrapper and edit component

---
+++ src/views/real-laboratories.vue
@@ -0,0 +1,15 @@
+<!--
+  Currently only a blanc wrapper.
+  Here we should add other content for adding,
+  editing and viewing real laboratories.
+-->
+
+<template>
+  <router-view />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({});
+</script>
