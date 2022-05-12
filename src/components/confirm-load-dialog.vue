6794ac9270f220516a9d5e933a1d2b0a6452bf92
andreashelms <helms@ubilabs.net>
2022-05-12 10:44:43 +0200

    feat(scenario-load): confirm scenario load dialog

---
+++ src/components/confirm-load-dialog.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-dialog v-model="open" max-width="600px">
+    <v-card>
+      <v-card-title>{{ $t('scenarios.loadScenario') }}</v-card-title>
+      <v-card-text>
+        <p>
+          {{ $t('scenarios.reallyLoadScenario') }}
+        </p>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" text @click="onCancel">
+          {{ $t('cancel') }}
+        </v-btn>
+        <v-btn color="blue darken-1" text @click="onLoad">
+          {{ $t('scenarios.loadScenario') }}
+          <template v-slot:loader>
+            <span>{{ $t('loading') }}</span>
+          </template>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  data() {
+    return {
+      open: true
+    };
+  },
+  methods: {
+    onLoad() {
+      this.$emit('onLoad');
+    },
+    onCancel() {
+      this.$emit('onCancel');
+    }
+  }
+});
+</script>
+
+<style scoped>
+.scenarios-list {
+  overflow: auto;
+  max-height: 50vh;
+}
+</style>
--- src/components/scenario-load-dialog.vue
+++ src/components/scenario-load-dialog.vue
@@ -1,73 +1,82 @@
 <template>
-  <v-dialog v-model="open" max-width="600px">
-    <template v-slot:activator="{on, attrs}">
-      <v-btn text v-bind="attrs" v-on="on">
-        {{ $t('scenarios.loadScenario') }}
-      </v-btn>
-    </template>
-
-    <v-card>
-      <v-card-title>{{ $t('scenarios.loadScenario') }}</v-card-title>
-      <v-card-text>
-        <v-list class="scenarios-list" two-line v-if="savedScenarios.length">
-          <v-list-item-group
-            v-model="selectedScenarioIndex"
-            active-class="primary--text"
-          >
-            <template v-for="(scenario, index) in savedScenarios">
-              <v-list-item :key="scenario.name">
-                <v-list-item-content>
-                  <v-list-item-title v-text="scenario.name"></v-list-item-title>
-
-                  <v-list-item-subtitle
-                    v-text="
-                      $tc(
-                        'scenarios.scenarioLayers',
-                        scenario.baseLayerTypes.length
-                      )
-                    "
-                  ></v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
-
-              <v-divider
-                v-if="index < savedScenarios.length - 1"
-                :key="index"
-              ></v-divider>
-            </template>
-          </v-list-item-group>
-        </v-list>
-
-        <p v-else class="grey--text">
-          {{ $t('scenarios.noSavedScenarios') }}
-        </p>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn color="blue darken-1" text @click="open = false">
-          {{ $t('cancel') }}
-        </v-btn>
-        <v-btn
-          color="blue darken-1"
-          text
-          @click="loadScenario()"
-          :loading="isLoading"
-          :disabled="selectedScenarioIndex == null || isLoading"
-        >
+  <div>
+    <ConfirmLoadDialog
+      v-if="showConfirm"
+      @onLoad="loadScenario"
+      @onCancel="showConfirm = false"
+    />
+    <v-dialog v-model="open" max-width="600px">
+      <template v-slot:activator="{on, attrs}">
+        <v-btn text v-bind="attrs" v-on="on">
           {{ $t('scenarios.loadScenario') }}
-          <template v-slot:loader>
-            <span>{{ $t('loading') }}</span>
-          </template>
         </v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+      </template>
+
+      <v-card>
+        <v-card-title>{{ $t('scenarios.loadScenario') }}</v-card-title>
+        <v-card-text>
+          <v-list class="scenarios-list" two-line v-if="savedScenarios.length">
+            <v-list-item-group
+              v-model="selectedScenarioIndex"
+              active-class="primary--text"
+            >
+              <template v-for="(scenario, index) in savedScenarios">
+                <v-list-item :key="scenario.name">
+                  <v-list-item-content>
+                    <v-list-item-title
+                      v-text="scenario.name"
+                    ></v-list-item-title>
+
+                    <v-list-item-subtitle
+                      v-text="
+                        $tc(
+                          'scenarios.scenarioLayers',
+                          scenario.baseLayerTypes.length
+                        )
+                      "
+                    ></v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-divider
+                  v-if="index < savedScenarios.length - 1"
+                  :key="index"
+                ></v-divider>
+              </template>
+            </v-list-item-group>
+          </v-list>
+
+          <p v-else class="grey--text">
+            {{ $t('scenarios.noSavedScenarios') }}
+          </p>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="open = false">
+            {{ $t('cancel') }}
+          </v-btn>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="!scenarioMetaData ? loadScenario() : (showConfirm = true)"
+            :loading="isLoading"
+            :disabled="selectedScenarioIndex == null || isLoading"
+          >
+            {{ $t('scenarios.loadScenario') }}
+            <template v-slot:loader>
+              <span>{{ $t('loading') }}</span>
+            </template>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import {mapActions} from 'vuex';
-import {MapActionsToMethods} from '@/types/store';
+import {mapActions, mapState} from 'vuex';
+import {MapActionsToMethods, MapStateToComputed} from '@/types/store';
 
 import {collection, getDocs} from 'firebase/firestore';
 import {database} from '../libs/firebase';
@@ -75,8 +84,11 @@ import {ANNRadarCollection} from '@/types/firestore';
 
 import {Scenario} from '@/types/scenarios';
 
+import ConfirmLoadDialog from './confirm-load-dialog.vue';
+
 interface Data {
   open: boolean;
+  showConfirm: boolean;
   isLoading: boolean;
   savedScenarios: Scenario[];
   selectedScenarioIndex: number | null;
@@ -85,10 +97,14 @@ export default Vue.extend({
   data(): Data {
     return {
       open: false,
+      showConfirm: false,
       isLoading: false,
       savedScenarios: [],
       selectedScenarioIndex: null
     };
+  },
+  components: {
+    ConfirmLoadDialog
   },
   watch: {
     async open(newOpenState: boolean) {
@@ -102,6 +118,9 @@ export default Vue.extend({
         this.resetDialog();
       }
     }
+  },
+  computed: {
+    ...(mapState as MapStateToComputed)('root', ['scenarioMetaData'])
   },
   methods: {
     ...(mapActions as MapActionsToMethods)('root', ['fetchScenarioDetails']),
@@ -131,6 +150,7 @@ export default Vue.extend({
       this.isLoading = false;
     },
     loadScenario() {
+      this.showConfirm = false;
       this.isLoading = true;
       const scenarioToSet =
         this.selectedScenarioIndex != null &&
