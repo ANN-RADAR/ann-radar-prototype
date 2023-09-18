cb761f079fafc6c65c281ccd28e4b783733e2dff
andreashelms <helms@ubilabs.net>
2023-09-18 15:22:32 +0200

    feat(i18n): load messages during runtime

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,10 +1,31 @@
-import de from './de.json';
-import en from './en.json';
+import VueI18n from 'vue-i18n/types';
+
+export const locales = {
+  en: 'English',
+  de: 'Deutsch',
+  dk: 'Danish',
+  es: 'Spanish',
+  lv: 'Latvian',
+  sv: 'Swedish'
+};
 
 export const fallbackLocale = 'en';
 
-export const messages = {
-  de,
-  // Locale terms shouldn't be translated, so we just add them to the fallback locale
-  en: {...en, locales: {en: 'English', de: 'Deutsch'}}
+export const getMessages = async (): Promise<VueI18n.LocaleMessages> => {
+  const messages = await Object.keys(locales).reduce<
+    Promise<VueI18n.LocaleMessages>
+  >(async (messages: Promise<VueI18n.LocaleMessages>, locale: string) => {
+    const previousMessages = await messages;
+
+    previousMessages[locale] = await (
+      await fetch(`i18n/${locale}.json`)
+    ).json();
+    return previousMessages;
+  }, Promise.resolve({}));
+
+  return {
+    ...messages,
+    // Locale terms shouldn't be translated, so we just add them to the fallback locale
+    en: {...messages.en, locales}
+  };
 };
--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,11 +1,18 @@
 import Vue from 'vue';
 import VueI18n from 'vue-i18n';
-import {fallbackLocale, messages} from '../i18n/index';
+import {fallbackLocale, getMessages} from '../i18n/index';
 
 Vue.use(VueI18n);
 
-export default new VueI18n({
+const vueI18n = new VueI18n({
   locale: fallbackLocale,
-  fallbackLocale: fallbackLocale,
-  messages
+  fallbackLocale: fallbackLocale
 });
+
+getMessages().then(messages =>
+  Object.keys(messages).forEach(locale =>
+    vueI18n.setLocaleMessage(locale, messages[locale])
+  )
+);
+
+export default vueI18n;
